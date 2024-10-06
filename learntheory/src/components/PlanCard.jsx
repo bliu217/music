34fdb0034c6641c7d55411a5fa1d2154c55ce094
bernard <bernardliu217@gmail.com>
2024-10-05 20:40:55 -0700

    Updated cards

--- learntheory/src/components/PlanCard.jsx
+++ learntheory/src/components/PlanCard.jsx
@@ -8,17 +8,23 @@ import {
   CardHeader,
 } from "@nextui-org/react";
 
-const PlanCard = ({ title, description }) => {
+const PlanCard = ({ title, description, image }) => {
   return (
-    <Card radius="lg" className="mr-5 justify-center text-center">
-      <CardHeader className=" bg-slate-500 px-20 py-5">
-        <h1 className="text-xl text-slate-100">{title}</h1>
+    <Card
+      radius="lg"
+      className="transition ease-in-out delay-150 border-solid border-2 border-black justify-center text-center flex-1 min-w-[200px] mx-10 shadow-mid-blue hover:border-yellow-300 duration-300"
+    >
+      <CardHeader className="flex flex-col bg-parch py-5 justify-center pb-20">
+        <h1 className="text-xl font-bold">{title}</h1>
+        <img src={image} className="w-auto h-28 mt-10" />
       </CardHeader>
-      <CardBody className="text-center">
+      <CardBody className="text-center border-t-2 border-black">
         <h3>{description}</h3>
       </CardBody>
-      <CardFooter className="justify-center mt-20">
-        <Button className="">START</Button>
+      <CardFooter className="justify-center">
+        <Button className="bg-bblue text-white font-outfit mb-5">
+          LEARN MORE
+        </Button>
       </CardFooter>
     </Card>
   );
--- learntheory/src/pages/HomePage.jsx
+++ learntheory/src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import myImage from "../assets/images/hero_image.png";
+import beginner from "../assets/images/beginner.png";
+import intermediate from "../assets/images/intermediate.png";
+import advanced from "../assets/images/advanced.png";
+
 import PlanCard from "../components/PlanCard";
 
 const HomePage = () => {
   return (
     <div className="pb-20">
-      <div className="mx-40 my-20 flex">
+      <div className="mx-40 my-20 flex z-1">
         <div>
           <br></br>
           <br></br>
@@ -14,20 +18,36 @@ const HomePage = () => {
           <br></br>
           <h1 className="text-6xl font-bold">MELODIFY</h1>
           <div className="font-poppins mt-10 text-2xl">
-            (Website name) offers a fun and interactive way to learn the
-            fundamentals of music. Whether you're a beginner or looking to
-            refine your skills, enjoy our range of engaging exercises, real-time
-            feedback, and visual aids, users can develop a deeper understanding
-            of music theory at their own pace.
+            Melodify offers a fun and interactive way to learn the fundamentals
+            of music. Whether you're a beginner or looking to refine your
+            skills, enjoy our range of engaging exercises, real-time feedback,
+            and visual aids, users can develop a deeper understanding of music
+            theory at their own pace.
           </div>
         </div>
-        <img src={myImage} alt="my-image" class="w-[500px] h-[500px] ml-10" />
+        <img
+          src={myImage}
+          alt="my-image"
+          class="w-[500px] h-[500px] ml-10 z-1"
+        />
       </div>
 
-      <div className="flex mx-20 mt-20 justify-center">
-        <PlanCard title="Beginner" description="starting out!" />
-        <PlanCard title="Intermediate" description="getting there!" />
-        <PlanCard title="Advanced" description="professional!" />
+      <div className="flex mx-40 mt-20 justify-center">
+        <PlanCard
+          title="Beginner"
+          description="notes, keys, rhythms, intervals"
+          image={beginner}
+        />
+        <PlanCard
+          title="Intermediate"
+          description="chord progressions, cadences"
+          image={intermediate}
+        />
+        <PlanCard
+          title="Advanced"
+          description="counterpoint, analysis, composition"
+          image={advanced}
+        />
       </div>
     </div>
   );
