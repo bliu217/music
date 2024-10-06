9af8c93f31fa25e12d2e1caa77fd60b4eeda0bf4
gjiang06 <gjiang06@students.cs.ubc.ca>
2024-10-06 00:37:46 -0700

    Pretty much identical home pages

--- learntheory/src/pages/HomePage.jsx
+++ learntheory/src/pages/HomePage.jsx
@@ -1,39 +1,42 @@
 import React from "react";
 import myImage from "../assets/images/hero_image.png";
+import logo from "../assets/images/logo.png";
 import beginner from "../assets/images/beginner.png";
 import intermediate from "../assets/images/intermediate.png";
 import advanced from "../assets/images/advanced.png";
+import { Button } from "@nextui-org/react";
 
 import PlanCard from "../components/PlanCard";
 
 const HomePage = () => {
   return (
     <div className="pb-20">
-      <div class="gradient-circle"> </div>
-      <div className="mx-40 my-20 flex">
-        <div>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <h1 className="text-6xl font-bold">MELODIFY</h1>
-          <div className="font-poppins mt-10 text-2xl">
+      <div class="gradient-circlel"> </div>
+
+
+    <div class="relative flex items-center mx-40 my-20 z-1">
+      <div>
+            <h1 class="text-6xl font-bold">MELODIFY</h1>
+            <h2 class= "text-4xl italic"> Music theory made easy</h2>
+            <p className="font-poppins mt-10 text-2xl z-1">
             Melodify offers a fun and interactive way to learn the fundamentals
             of music. Whether you're a beginner or looking to refine your
             skills, enjoy our range of engaging exercises, real-time feedback,
             and visual aids, users can develop a deeper understanding of music
             theory at their own pace.
+            </p>
+            <Button className="bg-bblue text-white font-outfit mb-5 mt-10 px-6 py-7 text-lg hover:bg-blue-600"> GET STARTED</Button>
           </div>
-        </div>
-        <img src={myImage} alt="my-image" class="w-[500px] h-[500px] ml-10" />
-      </div>
+      <img src={myImage} alt="my-image" class="w-[500px] h-[500px] ml-10 z-1" />
+        
+    </div>
 
       <div className="justify-center mt-32">
         <h1 className="text-center font-bold text-3xl">COURSE PLANS</h1>
         <p className="text-center mt-5 italic text-lg">
           choose your learning journey
         </p>
+        <div class="gradient-circler"> </div>
         <div className="flex mx-40 mt-20 justify-center">
           <PlanCard
             title="Beginner"
@@ -50,8 +53,18 @@ const HomePage = () => {
             description="counterpoint, analysis, composition"
             image={advanced}
           />
+          
         </div>
+      </div> 
+
+
+      <div class=" mt-40 border-t-1 border-t-black justify-between flex items-center"> 
+      <img src={logo} alt="my-image" class="w-50 h-auto ml-20 mt-10 z-1" />
+      <p class = "text-right mr-20 mt-10 font-poppins text-lg"> melodify@gmail.com <br></br>instagram <br></br>
+        linkedin
+      </p>
       </div>
+
     </div>
   );
 };
