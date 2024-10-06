caf9a14cb16c92fb786445f6f4bfa1e1acdfbc13
bernard <bernardliu217@gmail.com>
2024-10-06 11:29:47 -0700

    added progress card

--- learntheory/src/pages/Before.jsx
+++ learntheory/src/pages/Before.jsx
@@ -3,50 +3,60 @@ import SideBar from "../components/SideBar";
 import Topic from "../components/Topic";
 import Footer from "../components/Footer";
 import keyboard from "../assets/images/keyboard.png";
-import { Card, CardHeade } from "@nextui-org/react";
-
-
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import fullCircle from "../assets/misc/full.png";
+import listIcon from "../assets/misc/List.png";
+import partialCircle from "../assets/misc/partial.png";
+import emptyCircle from "../assets/misc/empty.png";
 
 const Before = () => {
   return (
     <div>
-
-    <div className="flex flex-col bg-parc justify-center items-center">
-        <div class = "mt-40"><h1 class = "font-bold text-6xl"> BEGINNER COURSE PLAN</h1></div>
+      <div className="flex flex-col bg-parc justify-center items-center">
+        <div class="mt-40">
+          <h1 class="font-bold text-6xl"> BEGINNER COURSE PLAN</h1>
+        </div>
         <br></br>
         <img
           src={keyboard}
           alt="my-image"
           class="w-[700px] h-auto mt-10 z-1 relative"
-          
         />
-    </div>
-    
-    <div class = "flex">
-        {/* <Card
-            radius="lg"
-            className="border-solid border-2 border-black w-fit shadow-mid-blue relative"
-          >
-            <CardHeader className="p-10 border-b-2 border-black">
-              <div className="relative">
-                <img src={noteLedger} className="h-[120px]" />
-                <img
-                  src={whole}
-                  className="absolute left-[70px] top-[40px] h-[28px]"
-                />
-              </div>
-            </CardHeader>
-            <CardBody>
-              <h3 className="font-bold text-xl mb-3">WHOLE NOTE</h3>
-              <ul className="list-disc list-inside px-5 w-[250px]">
-                <li>longest note duration</li>
-                <li>resembles an open note head with no stem</li>
-                <li>lasts 4 beats</li>
-              </ul>
-            </CardBody>
-          </Card> */}
-    </div>
+      </div>
 
+      <div class="flex">
+        <Card className="shadow-[-5px_5px_10px_0_rgba(0,0,0,0.75)] border-2 border-black">
+          <CardHeader className="items-start">
+            <img src={listIcon} className="w-[28px] mr-5" />
+            <div className="flex-col items-start w-[250px]">
+              <h1 className="font-bold text-xl">OVERVIEW</h1>
+              <div>module objectives, learning goals, course topics</div>
+            </div>
+          </CardHeader>
+          <CardBody>
+            <div className="flex items-center pt-2 border-t-2">
+              <img src={fullCircle} className="scale-75 mr-5" />
+              <h1 className="font-bold text-xl">INTRO TO BASICS</h1>
+            </div>
+            <div className="flex items-center pt-2 border-t-2">
+              <img src={partialCircle} className="scale-75 mr-5" />
+              <h1 className="font-bold text-xl">RHYTHM</h1>
+            </div>
+            <div className="flex items-center pt-2 border-t-2">
+              <img src={emptyCircle} className="scale-75 mr-5" />
+              <h1 className="font-bold text-xl">KEYS</h1>
+            </div>
+            <div className="flex items-center pt-2 border-t-2">
+              <img src={emptyCircle} className="scale-75 mr-5" />
+              <h1 className="font-bold text-xl">CHORDS</h1>
+            </div>
+            <div className="flex items-center pt-2 border-t-2">
+              <img src={emptyCircle} className="scale-75 mr-5" />
+              <h1 className="font-bold text-xl">INTERVALS</h1>
+            </div>
+          </CardBody>
+        </Card>
+      </div>
     </div>
   );
 };
