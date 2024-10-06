b2b2a7711413c045332443945ae73a17f3957cf5
bernard <bernardliu217@gmail.com>
2024-10-06 10:37:41 -0700

    animations

--- learntheory/src/components/Topic.jsx
+++ learntheory/src/components/Topic.jsx
@@ -46,6 +46,7 @@ const Topic = () => {
             src={ledger}
             className="p-16 w-[800px] h-auto z-20 object-cover"
           />
+
           <img
             src={treble}
             className="absolute top-[50px] left-[50px] z-0 h-[130px]"
@@ -54,48 +55,57 @@ const Topic = () => {
             src={fourFour}
             className="absolute top-[58px] left-[120px] z-0 h-[110px]"
           />
-          <img
-            src={whole}
-            className="absolute top-[117px] left-[180px] z-0 h-[28px]"
-          />
+
+          <a href="#notes">
+            <img
+              src={whole}
+              className="absolute top-[117px] left-[180px] z-0 h-[28px] transition-transform duration-300 transform hover:scale-125"
+            />
+          </a>
+
           <img
             src={measure}
             className="absolute top-[65px] left-[230px] z-0 h-[89px]"
           />
-
-          <img
-            src={half}
-            className="absolute top-[70px] left-[220px] z-0 h-[75px]"
-          />
-          <img
-            src={half}
-            className="absolute top-[70px] left-[270px] z-0 h-[75px]"
-          />
+          <a href="#notes">
+            <img
+              src={half}
+              className="absolute top-[70px] left-[220px] z-0 h-[75px] transition-transform duration-300 transform hover:scale-110"
+            />
+            <img
+              src={half}
+              className="absolute top-[70px] left-[270px] z-0 h-[75px] transition-transform duration-300 transform hover:scale-110"
+            />
+          </a>
 
           <img
             src={measure}
             className="absolute top-[65px] left-[340px] z-0 h-[89px]"
           />
+          <a href="#notes2">
+            <img
+              src={quarter}
+              className="absolute top-[68px] left-[330px] z-0 h-[75px] transition-transform duration-300 transform hover:scale-110"
+            />
+            <img
+              src={quarter}
+              className="absolute top-[68px] left-[380px] z-0 h-[75px] transition-transform duration-300 transform hover:scale-110"
+            />
+          </a>
 
-          <img
-            src={quarter}
-            className="absolute top-[68px] left-[330px] z-0 h-[75px]"
-          />
-          <img
-            src={quarter}
-            className="absolute top-[68px] left-[380px] z-0 h-[75px]"
-          />
-
-          <img
-            src={eight}
-            className="absolute top-[68px] left-[445px] z-0 h-[75px]"
-          />
+          <a href="#notes2">
+            <img
+              src={eight}
+              className="absolute top-[68px] left-[445px] z-0 h-[75px] transition-transform duration-300 transform hover:scale-105"
+            />
+          </a>
 
           <img
             src={measure}
             className="absolute top-[65px] left-[670px] z-0 h-[89px]"
           />
         </div>
+        <a id="notes" className="mt-10"></a>
       </Card>
       <div className="font-poppins font-bold text-xl/7 w-[800px] p-10 border-t-2 border-b-2 border-gray-800">
         Music notes are the symbols used to represent sounds in music. Each note
@@ -103,9 +113,10 @@ const Topic = () => {
         (how long the sound lasts). The basic notes include A, B, C, D, E, F,
         and G, which repeat in higher or lower octaves.
       </div>
-      <h3 className="font-bold text-3xl my-20">DIFFERENT NOTES</h3>
 
-      <div className="my-10 mx-20">
+      <section className="my-10 mx-20">
+        <a id="notes2" className="-mt-10"></a>
+
         <div className="grid grid-rows-2 grid-cols-2 gap-x-0 gap-y-8 bg-parch w-[600px] justify-items-center">
           <Card
             radius="lg"
@@ -193,7 +204,7 @@ const Topic = () => {
             </CardBody>
           </Card>
         </div>
-      </div>
+      </section>
 
       <div className="mt-24">
         <h1 className="text-dark-blue text-3xl font-bold mb-10">LOCK IT IN!</h1>
