58a19ac70d1c6bc2e9e655555a6ceb23743a1b5f
gjiang06 <gjiang06@students.cs.ubc.ca>
2024-10-06 10:06:13 -0700

    Footer 2

--- learntheory/src/pages/HomePage.jsx
+++ learntheory/src/pages/HomePage.jsx
@@ -62,14 +62,14 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div class=" mt-40 border-t-1 border-t-black justify-between flex items-center">
+      {/* <div class=" mt-40 border-t-1 border-t-black justify-between flex items-center">
         <img src={logo} alt="my-image" class="w-50 h-auto ml-20 mt-10 z-1" />
         <p class="text-right mr-20 mt-10 font-poppins text-lg">
           {" "}
           melodify@gmail.com <br></br>instagram <br></br>
           linkedin
         </p>
-      </div>
+      </div> */}
   
 
     </div>
