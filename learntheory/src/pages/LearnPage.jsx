acbf8c8063195544106c80304112712280a51c64
gjiang06 <gjiang06@students.cs.ubc.ca>
2024-10-06 10:04:53 -0700

    Footer

--- learntheory/src/pages/LearnPage.jsx
+++ learntheory/src/pages/LearnPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SideBar from "../components/SideBar";
 import Topic from "../components/Topic";
+import Footer from "../components/Footer";
 
 const LearnPage = () => {
   return (
     <div>
       <SideBar />
       <Topic />
+
     </div>
   );
 };
